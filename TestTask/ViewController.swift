f4b8833f65f5cda688cfaea43f443b074ca849d4
Dmitry <Atlantiskun@gmail.com>
2022-04-01 08:07:59 +0700

    initial commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Дмитрий Болучевских on 01.04.2022.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
